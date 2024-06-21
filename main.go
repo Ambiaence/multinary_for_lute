794c037d7fa2f297a9e1d228dcecfb8eb3f70f72
George Kent Allen <main@georgeallen.net>
2024-06-21 08:06:55 -0700

    Basically done but quaulity of life improvements could be made.

--- main.go
+++ main.go
@@ -2,39 +2,40 @@ package main
 
 import (
 	"fmt"
-	"os"
 	"log"
 	"net/http"
 )
 
-type Page struct {
-	Title string
-	Body []byte
-}
+var elements []string
 
-func (page * Page) save() error {
-	filename := page.Title + ".txt" //Remember to refer to page.Title 
-	return os.WriteFile(filename, page.Body, 0600)
-}
-
-func load_page(title string) (*Page, error){
-	filename := title + ".txt"
-	body, error := os.ReadFile(filename)
-
-	if error != nil {
-		return nil, error
-	}
-
-	return &Page{Title: title, Body: body}, nil
-}
 
 func view_handler(writer http.ResponseWriter, request *http.Request) {
-	title := request.URL.Path[len("/view/"):]
-	page, _ := load_page(title)
-	fmt.Fprintf(writer, "<h1>%s</h1> <div>%s</div>", page.Title, page.Body) // It's FPrintf
+	word := request.URL.Path[len("/word/"):]
+	fmt.Fprint(writer, "<h1>how about now </h1><body>\n") 
+	for _, element := range elements {
+		fmt.Fprintf(writer, element, word)
+	}
+}
+
+func construct_embedded_element(website string) string {
+	element := ""
+	element +="<embed src = \""
+	element += website
+	element += "%s\" "
+	element += "width=\"1000\" height = \"400\" ></embed>\n"
+	return element
+}
+
+func add_website_to_elements(elements []string, website string) []string {
+	element := construct_embedded_element(website)
+	elements = append(elements, element)
+	return elements 
 }
 
 func main() {
+	elements = add_website_to_elements(elements, "https://context.reverso.net/translation/german-english/")
+	elements = add_website_to_elements(elements, "https://www.dict.cc/?s=")
+
 	http.HandleFunc("/view/", view_handler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
